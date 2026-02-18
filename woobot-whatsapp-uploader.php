c223a6a8d25ddaf76722af5ac49727e713af4172
Noy Atar <noy@woobot.co.il>
2026-02-18 20:38:49 +0000

    v1.1.2 - Bump version for cache busting

--- woobot-whatsapp-uploader.php
+++ woobot-whatsapp-uploader.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'WOOBOT_VERSION', '1.1.0' );
+define( 'WOOBOT_VERSION', '1.1.2' );
 define( 'WOOBOT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'WOOBOT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'WOOBOT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
